d579b7bf1242e5dbae29fae7051496922171ddd5
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-09-20 18:44:07 -0300

    feat: adds render structure to task-form component

---
+++ src/components/TaskForm/TaskForm.tsx
@@ -0,0 +1,27 @@
+import { StyleSheet, View } from "react-native";
+import { BaseButton } from "../BaseButton";
+import { Field } from "../Field";
+
+export const TaskForm = () => {
+	return (
+		<View style={styles.container}>
+			<Field
+				labelText="Título"
+				nativeID="task-title"
+				placeholder="Ex: Passear com os cachorros"
+			/>
+			<Field
+				labelText="Descrição"
+				nativeID="task-title"
+				placeholder="Ex: Parque do centro"
+			/>
+			<BaseButton>Adicionar</BaseButton>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		gap: 20,
+	},
+});
